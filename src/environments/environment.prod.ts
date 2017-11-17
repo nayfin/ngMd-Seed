534f70f00c23e7252f8ca264835b7909d11a485a
Nathan Kern <natjkern@gmail.com>
2017-11-17 00:36:34 -0600

    hopefully completed PWA reqs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: ""
+    apiKey: "AIzaSyAfKGzC-xdoA363ipOKD69JdA6obGQxVPM",
+    authDomain: "tft-materialseed.firebaseapp.com",
+    databaseURL: "https://tft-materialseed.firebaseio.com",
+    projectId: "tft-materialseed",
+    storageBucket: "tft-materialseed.appspot.com",
+    messagingSenderId: "1015795404760"
   }
 };
